08c79bfc93f7ad781f164f2093abadda10619f47
Aris Ceballos <arisceballos@macbook-air.mynetworksettings.com>
2022-05-30 11:48:37 -0400

    Using Redux

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -18,6 +18,20 @@ import { createDrawerNavigator, DrawerItems } from "react-navigation-drawer";
 import { createAppContainer } from "react-navigation";
 import { Icon } from "react-native-elements";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { connect } from "react-redux";
+import {
+  fetchCampsites,
+  fetchComments,
+  fetchPromotions,
+  fetchPartners,
+} from "../redux/ActionCreators";
+
+const mapDispatchToProps = {
+  fetchCampsites,
+  fetchComments,
+  fetchPromotions,
+  fetchPartners,
+};
 
 const ContactNavigator = createStackNavigator(
   {
@@ -138,11 +152,11 @@ const CustomDrawerContentComponent = (props) => (
             style={styles.drawerImage}
           />
         </View>
-        <View style={{flex: 2}}>
-            <Text style={styles.drawerHeaderText}>NuCamp</Text>
+        <View style={{ flex: 2 }}>
+          <Text style={styles.drawerHeaderText}>NuCamp</Text>
         </View>
       </View>
-      <DrawerItems {...props}/>
+      <DrawerItems {...props} />
     </SafeAreaView>
   </ScrollView>
 );
@@ -202,6 +216,13 @@ const MainNavigator = createDrawerNavigator(
 const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
+  componentDidMount() {
+    this.props.fetchCampsites(),
+      this.props.fetchComments(),
+      this.props.fetchPromotions(),
+      this.props.fetchPartners();
+  }
+
   render() {
     return (
       <View
@@ -217,32 +238,32 @@ class Main extends Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    drawerHeader: {
-        backgroundColor: '#5637DD',
-        height: 140,
-        alignItems: 'center',
-        justifyContent: 'center',
-        flex: 1,
-        flexDirection: 'row'
-    },
-    drawerHeaderText: {
-        color: '#fff',
-        fontSize: 24,
-        fontWeight: 'bold'
-    },
-    drawerImage: {
-        margin: 10,
-        height: 60,
-        width: 60
-    },
-    stackIcon: {
-        marginLeft: 10,
-        color: '#fff',
-        fontSize: 24
-    }
+  container: {
+    flex: 1,
+  },
+  drawerHeader: {
+    backgroundColor: "#5637DD",
+    height: 140,
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    flexDirection: "row",
+  },
+  drawerHeaderText: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  drawerImage: {
+    margin: 10,
+    height: 60,
+    width: 60,
+  },
+  stackIcon: {
+    marginLeft: 10,
+    color: "#fff",
+    fontSize: 24,
+  },
 });
 
-export default Main;
+export default connect(null, mapDispatchToProps)(Main);
